4cc025c0df7087bb4b7aca651f7b19926f30cf2a
Povshy <vladcarrib@gmail.com>
2024-02-23 06:05:05 +0200

    222

---
+++ src/container/test2/index.js
@@ -0,0 +1,16 @@
+import {
+  createElement,
+  createHeader,
+} from '../../script/layout'
+
+const page = document.querySelector('.page')
+
+const header = createHeader()
+
+page.append(header)
+
+const title = createElement('h1', 'title', 'Коммьюніті')
+
+page.append(title)
+
+// =============
--- src/route/index.js
+++ src/route/index.js
@@ -5,9 +5,11 @@ const router = express.Router()
 // Підключіть файли роутів
 const test = require('./test')
 // Підключіть інші файли роутів, якщо є
+const test2 = require('./test2')
 
 // Об'єднайте файли роутів за потреби
 router.use('/', test)
+router.use('/test2', test2)
 // Використовуйте інші файли роутів, якщо є
 
 // Експортуємо глобальний роутер
